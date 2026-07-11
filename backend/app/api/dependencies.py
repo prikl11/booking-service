90c2400bd0a8d1da820393992223373e750a9cf5
artem <artem.serdyukow@gmail.com>
2026-07-11 19:19:54 +0300

    feat: add current user functions

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -3,6 +3,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
 
 from app.database.db import get_db
+from app.utils.security import get_current_user
+from app.database.models import User
 
 
 SessionDep = Annotated[AsyncSession, Depends(get_db)]
+CurrentUserDep = Annotated[User, Depends(get_current_user)]
--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -2,12 +2,17 @@ from pwdlib import PasswordHash
 from pwdlib.hashers.argon2 import Argon2Hasher
 from datetime import datetime, timedelta, timezone
 from jwt import PyJWTError, ExpiredSignatureError
-from fastapi import HTTPException, status
+from fastapi import HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from typing import Annotated
 import jwt
 
 from app.utils.config import settings
+from app.crud.users import get_user_by_id
+from app.api.dependencies import SessionDep
 
 password_hash = PasswordHash((Argon2Hasher(),))
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 def hash_password(password: str) -> str:
     """Hash a password with Argon2"""
@@ -70,3 +75,16 @@ def decode_refresh_token(token: str) -> dict:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Refresh token expired")
     except PyJWTError:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid refresh token")
+
+
+async def get_current_user(db: SessionDep, token: Annotated[str, Depends(oauth2_scheme)]):
+    payload = decode_access_token(token=token)
+    user_id = payload.get("sub")
+    if user_id is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token")
+    
+    user = await get_user_by_id(db=db, user_id=int(user_id))
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User not found")
+    
+    return user
